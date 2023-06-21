f891d3249dd4377c4af2c65f138a23c5f48a97f5
Michael Mironychev <michael.mironychev@gmail.com>
2023-06-21 11:06:48 +0700

    @packages/tools :hot_pepper: update

--- packages/react-tools/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rootDir: './',
-  testEnvironment: 'jest-environment-jsdom',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testMatch: ['**/?(*.)test.[jt]s?(x)']
-};
